bfdfec980aa3d21b9d5ef1000094b283b7d6a29c
CagriAvsar <c.avsar@hotmail.com>
2022-06-06 00:40:19 +0200

    logIn style demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 
 // AUTH
+import { AuthService } from './shared/services/auth.service';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
@@ -13,12 +14,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
+
+// SIGN IN
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SignInComponent } from './components/signing/sign-in/sign-in.component';
 import { SignUpComponent } from './components/signing/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './components/signing/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/signing/verify-email/verify-email.component';
-import { AuthService } from './shared/services/auth.service';
+// SIGN IN END
+
+// MATERIAL
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -37,7 +43,8 @@ import { AuthService } from './shared/services/auth.service';
     AngularFireAuthModule,
     AngularFireStorageModule,
     AngularFirestoreModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    MatInputModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
